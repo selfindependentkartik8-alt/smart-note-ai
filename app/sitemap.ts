fb0666af81caab00af165c9d8c152a0efe70c3e4
Krish <selfindependentkartik8@gmail.com>
2026-08-29 11:37:28 +0530

    Updated SEO

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://smart-note-btvps5422-krish-aiw-orks.vercel.app/",
+      url: "https://smart-note-btvps5422-krish-aiw-orks.vercel.app",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
